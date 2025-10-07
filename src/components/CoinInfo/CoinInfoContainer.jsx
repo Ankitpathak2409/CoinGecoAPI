ad68c5fc30524f080865d19b96c07f6a07b7f2c7
Ankit Pathak <pathaka9456@gmail.com>
2025-10-07 10:01:29 +0530

    Code Cleanup: Added custom hooks

--- src/components/CoinInfo/CoinInfoContainer.jsx
+++ src/components/CoinInfo/CoinInfoContainer.jsx
@@ -1,24 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+
 import CoinInfo from "./CoinInfo";
-import currencyStore from '../../zustand/store';
-import { useState } from "react";
-import { fetchCoinHistoricData } from "../../Services/fetchCoinHistoricData";
 import PageLoader from '../PageLoader/PageLoader';
 import Alert from "../Alert/Alert";
+import useFetchCoinHistory from "../../hooks/useFetchCoinHistory";
 
 
 function CoinInfoContainer({coinId}){
 
-  const { currency } = currencyStore();
-  const [days, setDays] = useState(7);
-  const [interval, setCoinInterval] = useState('daily');
+  const [ historicData, isLoading, isError, days, setDays, setCoinInterval, currency ] = useFetchCoinHistory(coinId);
 
-  const {data : historicData, isLoading, isError } = useQuery({
-    queryKey: ['coinHistoricData', coinId, currency, days, interval], 
-    queryFn: () => fetchCoinHistoricData(coinId, interval, days, currency ),
-    cacheTime: 1000*60*2,
-    staleTime: 1000*60*2,
-});
+ 
 
 if(isLoading){
   return <PageLoader />
